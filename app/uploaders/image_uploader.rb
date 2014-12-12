4048b2376b441d91a5df98380ed668cb9e268b6a
Jatin Bhandari <jatin.bhandari@mail.utoronto.ca>
2014-12-12 00:31:11 -0500

    Adds gallery and image views

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,8 +9,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  version :thumb do
-    process :resize_to_fill => [200, 200]
-  end
 end
